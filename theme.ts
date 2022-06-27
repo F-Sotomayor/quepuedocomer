a6e5e11419ed8d2081e798cea260bda1a73a5f5c
F-Sotomayor <MY_NAME@example.com>
2022-06-27 19:16:40 -0300

    project setup

---
+++ theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+import { theme } from "@chakra-ui/theme";
+
+export default extendTheme({
+  colors: {
+    primary: theme.colors.facebook,
+  },
+});
