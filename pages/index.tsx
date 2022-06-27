06552f771059b2d16f4ce7ff2185a74f51249860
F-Sotomayor <MY_NAME@example.com>
2022-06-27 19:30:18 -0300

    darkmode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import { Button, Heading, Stack, useColorMode } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <Stack>
       <Heading>holi</Heading>
+      <Button w={128} onClick={toggleColorMode}>
+        oli
+      </Button>
     </Stack>
   );
 };
--- theme.ts
+++ theme.ts
@@ -2,6 +2,8 @@ import { extendTheme } from "@chakra-ui/react";
 import { theme } from "@chakra-ui/theme";
 
 export default extendTheme({
+  initialColorMode: "light",
+  useSystemColorMode: false,
   colors: {
     primary: theme.colors.facebook,
   },
